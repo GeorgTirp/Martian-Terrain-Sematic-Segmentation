368816cc1b351b4a43ac0e8382a7fe9cd788175e
Georg Tirpitz <georg.tirpitz@student.uni-tuebingen.de>
2025-11-20 20:52:31 +0100

    pushing the conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-# Point to repo root and src
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 SRC = os.path.join(ROOT, "src")
 sys.path.insert(0, SRC)
@@ -15,4 +14,8 @@ autodoc_mock_imports = [
     "torch",
     "torchvision",
     "pytorch_lightning",
+    "numpy",
+    "matplotlib",
+    "PIL",
+    "sklearn",
 ]
