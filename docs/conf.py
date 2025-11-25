e95e50f13120ec24a9046d7cb7a4292c0f6ca7ca
Georg Tirpitz <georg.tirpitz@student.uni-tuebingen.de>
2025-11-25 20:15:08 +0100

    added in the conf.py

---
+++ docs/conf.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+SRC = os.path.join(ROOT, "src")
+sys.path.insert(0, SRC)
+
+#extensions = [
+#    "sphinx.ext.autodoc",
+#    "sphinx.ext.napoleon",
+#]
+#
+#autodoc_mock_imports = [
+#    "torch",
+#    "torchvision",
+#    "pytorch_lightning",
+#    "numpy",
+#    "matplotlib",
+#    "PIL",
+#    "sklearn",
+#    "cv2",
+#    "albumentations",
+#    "torchmetrics",
+#    # add any other heavy deps used only in your code
+#]
